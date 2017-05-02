ac01319724e918fd464f3a5efe69ab3196478041
TallesFaria <tallesbeginsc@gmail.com>
2017-05-02 10:36:10 -0300

    Reducer Composition!

--- src/todos.js
+++ src/todos.js
@@ -1,18 +1,38 @@
 import deepFreeze from 'deep-freeze'
 import expect from 'expect'
 
+const todo = (state, action) => {
+    switch (action.type) {
+        case 'ADD_TODO':
+            return {
+                id: action.id,
+                text: action.text,
+                completed: false
+            }   
+        case 'TOGGLE_TODO':
+            if(todo.id !== action.id) {
+                return todo
+            }
+            return {
+                ...todo, 
+                completed: !todo.completed
+            }       
+        default:
+            return state
+    }
+}
+
 
 const todos = (state = [], action) => {
     switch (action.type) {
         case 'ADD_TODO':
             return [
                 ...state,
-                {
-                    id: action.id,
-                    text: action.text,
-                    completed: false
-                }
-            ] 
+                todo(undefined, action)
+                
+            ]
+        case 'TOGGLE_TODO':
+            return state.map((t) => todo(t, action))      
         default:
             return state
     }
@@ -39,5 +59,42 @@ const testeAddTodo = () => {
     expect(todos(stateBefore, action)).toEqual(stateAfter)
 }
 
+const testeToggleTodo = () => {
+    const stateBefore = [
+        {
+            completed: false,
+            id: 0,
+            text: 'Learn Redux'    
+        },
+        {
+            completed: false,
+            id: 1,
+            text: 'Go Shopping!'    
+        }
+    ]
+    const action = {
+        type: 'TOGGLE_TODO',
+        id: 1
+    }
+    const stateAfter = [
+        {
+            completed: false,
+            id: 0,
+            text: 'Learn Redux'    
+        },
+        {
+            completed: true,
+            id: 1,
+            text: 'Go Shopping!'    
+        }
+    ]
+    
+    deepFreeze(stateBefore)
+    deepFreeze(action)
+
+    expect(todos(stateBefore, action)).toEqual(stateAfter)
+}
+
 testeAddTodo()
+testeToggleTodo()
 console.log('All tests passed!')
