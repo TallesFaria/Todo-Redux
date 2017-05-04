42bcec317fa2064976ac002077ca36dd2b88a2d0
TallesFaria <tallesbeginsc@gmail.com>
2017-05-04 16:27:57 -0300

    getVisibleTodos moved into VisibleTodoList

--- src/components/VisibleTodoList.js
+++ src/components/VisibleTodoList.js
@@ -1,7 +1,19 @@
 import TodoList from './TodoList'
 import { connect } from 'react-redux'
-import getVisibleTodos from '../reducers/getVisibleTodos'
 import { toggleTodo } from '../actions/index'
+
+const getVisibleTodos = (todos, filter) => {
+  switch (filter) {
+    case 'SHOW_ALL':
+      return todos
+    case 'SHOW_ACTIVE':
+      return todos.filter(t => !t.completed)
+    case 'SHOW_COMPLETED':
+      return todos.filter(t => t.completed)
+    default:
+      break;
+  }
+}
 
 const mapStateToProps = (state) => ({
     todos: getVisibleTodos(state.todos, state.visibilityFilter) 
