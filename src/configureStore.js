54873428b73d5c5bfa37c97208d3cb2a9920b006
TallesFaria <tallesbeginsc@gmail.com>
2017-05-10 19:17:56 -0300

    Normalizing the state shape - looking like database!

--- src/configureStore.js
+++ src/configureStore.js
@@ -3,9 +3,23 @@ import { loadState, saveState } from './localStorage'
 import throttle from 'lodash/throttle'
 import todoApp from './reducers'
 
+// const addLoggintToDispatch = (store) => {
+//     const rawDispatch = store.dispatch
+//     return(action) => {
+//         console.group(action.type)
+//         console.log('prev state', store.getState())
+//         console.log('action', action)
+//         const returnValue = rawDispatch(action)
+//         console.groupEnd(action.type)
+//     }
+// }
+
 const configureStore = () => {
     const persistedState = loadState()
     const store = createStore(todoApp, persistedState)
+
+    // //store.dispatch = addLogginToDispatch(store)
+
     store.subscribe(throttle(() => {
     saveState({
         todos: store.getState().todos
--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,23 +1,5 @@
-const todo = (state, action) => {
-  switch (action.type) {
-      case 'ADD_TODO':
-          return {
-              id: action.id,
-              text: action.text,
-              completed: false
-          }   
-      case 'TOGGLE_TODO':
-          if(state.id !== action.id) {
-              return state
-          }
-          return {
-              ...state, 
-              completed: !state.completed
-          }       
-      default:
-          return state
-  }
-}
+import { combineReducers } from 'redux'
+import todo from './todo'
 
 const byId = (state = {}, action) => {
     switch (action.type) {
@@ -32,16 +14,30 @@ const byId = (state = {}, action) => {
     }
 }
 
-export default byId
+const allIds = (state = [], action) => {
+    switch (action.type) {
+        case 'ADD_TODO':
+            return [...state, action.id]
+        default:
+            return state
+    }
+}
+
+const todos =  combineReducers({ byId, allIds })
+
+export default todos
+
+const getAllTodos = (state) => state.allIds.map((id) => state.byId[id])
 
 export const getVisibleTodos = (state, filter) => {
+  const allTodos = getAllTodos(state)
   switch (filter) {
     case 'all':
-      return state
+      return allTodos
     case 'active':
-      return state.filter(t => !t.completed)
+      return allTodos.filter(t => !t.completed)
     case 'completed':
-      return state.filter(t => t.completed)
+      return allTodos.filter(t => t.completed)
     default:
       throw new Error(`Unknown filter: ${filter}.`)
   }
