6719bc901598725c1b896360d75864ce967dd47a
TallesFaria <tallesbeginsc@gmail.com>
2017-05-11 15:32:36 -0300

    Just minor adjusts!!

--- src/actions/index.js
+++ src/actions/index.js
@@ -16,7 +16,7 @@ export const fetchTodos = (filter) => (dispatch, getState) => {
   return api.fetchTodos(filter).then(response =>
     {
       dispatch({
-        type: 'FETCH_TODOS_SUCESS',
+        type: 'FETCH_TODOS_SUCCESS',
         filter,
         response: normalize(response, schema.arryOfTodos)
       })
@@ -34,7 +34,7 @@ export const fetchTodos = (filter) => (dispatch, getState) => {
 export const addTodo = (text) => (dispatch) => 
   api.addTodo(text).then(response => {
     dispatch({
-      type: 'ADD_TODO_SUCESS',
+      type: 'ADD_TODO_SUCCESS',
       response: normalize(response, schema.todo)
     })
   })
@@ -42,11 +42,7 @@ export const addTodo = (text) => (dispatch) =>
 export const toggleTodo = (id) => (dispatch) => 
   api.toggleTodo(id).then(response => {
     dispatch({
-      type: 'TOGGLE_TODO_SUCESS',
+      type: 'TOGGLE_TODO_SUCCESS',
       response: normalize(response, schema.todo)
     })
   })
-
-
-  
-
