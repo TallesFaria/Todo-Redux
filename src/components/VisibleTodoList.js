1c36422e3e0d48006a4f49fb299f2c0e831e9c0e
TallesFaria <tallesbeginsc@gmail.com>
2017-05-10 21:24:27 -0300

    Wrapping dispatch() to recognize promises!!!

--- src/components/VisibleTodoList.js
+++ src/components/VisibleTodoList.js
@@ -4,7 +4,6 @@ import { connect } from 'react-redux'
 import * as actions from '../actions'
 import { withRouter } from 'react-router'
 import { getVisibleTodos } from '../reducers'
-import { fetchTodos } from '../api'
 
 class VisibleTodoList extends Component {
     componentDidMount() {
@@ -18,10 +17,8 @@ class VisibleTodoList extends Component {
     }
 
     fetchData() {
-        const { filter, receiveTodos } = this.props
-        fetchTodos(filter).then(todos => 
-            receiveTodos(todos)
-        )
+        const { filter, fetchTodos } = this.props
+        fetchTodos(filter)
     }
 
     render(){
